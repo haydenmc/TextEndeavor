6de4e8d63abd0d4124aaf9bd686431f902e01ebb
Hayden McAfee <hayden@outlook.com>
2018-08-31 23:14:48 -0700

    A start

---
+++ Hubs/GameHub.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+namespace TextEndeavor.Hubs
+{
+    public class GameHub: Hub
+    {
+        public async Task SendCommand(string command)
+        {
+            await Clients.Caller.SendAsync("CommandResponse");
+        }
+    }
+}
